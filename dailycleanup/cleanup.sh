e13b85927ce072c9335cdbd7bf6864fb2cab9909
Suhail Nepal <suhail_nepal@outlook.com>
2023-02-05 21:46:14 +1100

    Add cleanup script

---
+++ dailycleanup/cleanup.sh
@@ -0,0 +1,11 @@
+#Date: 05/02/2023
+
+#!/bin/bash
+
+# Get a list of all resource groups in the subscription
+resource_groups=$(az group list --query "[].name" -o tsv)
+
+# Loop through each resource group and delete it
+for resource_group in $resource_groups; do
+  az group delete --name $resource_group --yes
+done
